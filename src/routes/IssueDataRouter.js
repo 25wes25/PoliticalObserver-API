37d3ccd24ca6dff33a25fd95320b3d7e028c7b2c
Pardis <pardis.khodadoustan@student.csulb.edu>
2020-05-04 03:20:54 -0700

    get issueData by education and modify gender and party

--- src/routes/IssueDataRouter.js
+++ src/routes/IssueDataRouter.js
@@ -6,11 +6,18 @@ const IssueDataModel = require('../models/issueData');
 
 router.get('/issuedata/gender/issueid/:issueid', getIssueDataGenderByIssueId);
 router.get('/issuedata/party/issueid/:issueid', getIssueDataPartyByIssueId);
+router.get('/issuedata/education/issueid/:issueid', getIssueDataEducationByIssueId);
 
 // http status codes
 const statusOK = 200;
 const statusNotFound = 404;
 const statusError = 500;
+const genderKeys = ['Male', 'Female', 'Other'];
+const genderValues = ['male', 'female', 'other'];
+const partyKeys = ['Democrat', 'Republican', 'Libertarian', 'Green', 'Constitution', 'Unaligned'];
+const partyValues = ['democrat', 'republican', 'libertarian', 'green', 'constitution', 'unaligned'];
+const educationKeys = ['None', 'Diploma', 'Associates', 'Bachelors', 'Masters', 'Doctoral'];
+const educationValues = ['none', 'diploma', 'associates', 'bachelors', 'masters', 'doctoral'];
 
 async function getIssueDataGenderByIssueId(request, response, next) {
     try {
@@ -18,12 +25,12 @@ async function getIssueDataGenderByIssueId(request, response, next) {
         let issueDataGraphFormat = {};
         if(issueDataRes.length>0) {
             let issueData = issueDataRes[0];
-            let genderYes = issueData.yes.gender;
-            let genderNo = issueData.no.gender;
-            issueDataGraphFormat = {
-                gender:[{x:'Male', y: genderNo.Male}, {x:'Female', y: genderNo.Female}, {x:'Other', y: genderNo.Other},
-                    {x:'Male', y: genderYes.Male}, {x:'Female', y: genderYes.Female}, {x:'Other', y: genderYes.Other}]
-            }
+            let genderConcat = [];
+            for(let i = 0; i<genderKeys.length; i++)
+                genderConcat.push({x:genderValues[i], y: issueData.no.gender[genderKeys[i]]});
+            for(let i = 0; i<genderKeys.length; i++)
+                genderConcat.push({x:genderValues[i], y: issueData.yes.gender[genderKeys[i]]});
+            issueDataGraphFormat = {gender:genderConcat};
         }
         response.statusCode = statusOK;
         response.send(issueDataGraphFormat);
@@ -35,18 +42,36 @@ async function getIssueDataGenderByIssueId(request, response, next) {
 async function getIssueDataPartyByIssueId(request, response, next) {
     try {
         let issueDataRes = await IssueDataModel.find({issueId: String(request.params.issueid).toLowerCase()}).exec();
-        //console.log(issueDataRes);
-        let issueDataGraphFormat = {}
+        let issueDataGraphFormat = {};
         if(issueDataRes.length>0) {
             let issueData = issueDataRes[0];
-            let partyYes = issueData.yes.party;
-            let partyNo = issueData.no.party;
-            issueDataGraphFormat = {
-                party:[{x:'Democrat', y: partyNo.Democrat}, {x:'Republican', y: partyNo.Republican}, {x:'Libertarian', y: partyNo.Libertarian},
-                    {x:'Green', y: partyNo.Green}, {x:'Constitution', y: partyNo.Constitution}, {x:'Unaligned', y: partyNo.Unaligned},
-                    {x:'Democrat', y: partyYes.Democrat}, {x:'Republican', y: partyYes.Republican}, {x:'Libertarian', y: partyYes.Libertarian},
-                    {x:'Green', y: partyYes.Green}, {x:'Constitution', y: partyYes.Constitution}, {x:'Unaligned', y: partyYes.Unaligned}]
-            }
+            let partyConcat = [];
+            for(let i = 0; i<partyKeys.length; i++)
+                partyConcat.push({x:partyValues[i], y: issueData.no.party[partyKeys[i]]});
+            for(let i = 0; i<partyKeys.length; i++)
+                partyConcat.push({x:educationValues[i], y: issueData.yes.party[partyKeys[i]]});
+            issueDataGraphFormat = {party:partyConcat};
+        }
+        response.statusCode = statusOK;
+        response.send(issueDataGraphFormat);
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function getIssueDataEducationByIssueId(request, response, next) {
+    try {
+        let issueDataRes = await IssueDataModel.find({issueId: String(request.params.issueid).toLowerCase()}).exec();
+        console.log(issueDataRes);
+        let issueDataGraphFormat = {};
+        if(issueDataRes.length>0) {
+            let issueData = issueDataRes[0];
+            let educationConcat = [];
+            for(let i = 0; i<educationKeys.length; i++)
+                educationConcat.push({x:educationValues[i], y: issueData.no.education[educationKeys[i]]});
+            for(let i = 0; i<educationKeys.length; i++)
+                educationConcat.push({x:educationValues[i], y: issueData.yes.education[educationKeys[i]]});
+            issueDataGraphFormat = {education:educationConcat};
         }
         response.statusCode = statusOK;
         response.send(issueDataGraphFormat);
