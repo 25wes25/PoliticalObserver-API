b79cfb2df35fdfee243a49379eaf8b6174e296b8
Pardis <pardis.khodadoustan@student.csulb.edu>
2020-05-10 02:30:33 -0700

    Getting recent notifications not working properly

--- src/routes/NotificationRouter.js
+++ src/routes/NotificationRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const NotificationModel = require('../models/notification');
 
 router.get('/notification/:id', getNotificationById);
 router.get('/notification/', getAllNotifications);
+router.get('/notification/recent/:days', getRecentNotifications);
 router.post('/notification/', createNotification);
 
 // http status codes
@@ -13,10 +15,10 @@ const statusOK = 200;
 const statusNotFound = 404;
 const statusError = 500;
 
-
 async function createNotification(request, response, next) {
     let body = request.body;
-    body.date = new Date();
+    //body.date = moment().subtract(10, 'days'); //just for testing with different dates
+    body.date = moment();
     const notification = new NotificationModel(body);
     notification.save(async (err, dbRes) => {
         if (err) return console.error(err);
@@ -45,5 +47,20 @@ async function getAllNotifications(request, response, next) {
         next(e);
     }
 }
+
+async function getRecentNotifications(request, response, next) {
+    const start = moment().subtract(request.params.days, "days");
+    try {
+        let notifications = await NotificationModel.find({date: {
+                $gte: start
+            }}).exec();
+        response.statusCode = statusOK;
+        response.send(notifications);
+    } catch (e) {
+        next(e);
+    }
+}
+
+
 
 module.exports = router;
