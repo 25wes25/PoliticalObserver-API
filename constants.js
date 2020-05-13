f4fbf1d1058554e20efb55b39a066682e5a3c05f
Wesley Swanson <wesleyswanson97@gmail.com>
2020-05-12 22:53:23 -0700

    Removed state from demographicsList constant

--- constants.js
+++ constants.js
@@ -25,7 +25,6 @@ const demographicsList = [
     {type: 'Age', key: 'age', data: age},
     {type: 'Gender', key: 'gender', data: gender},
     {type: 'Marital Status', key: 'maritalStatus', data: maritalStatus},
-    {type: 'State', key: 'state', data: state},
     {type: 'Ethnicity', key: 'ethnicity', data: ethnicity},
     {type: 'Education', key: 'education', data: education},
     {type: 'Income', key: 'income', data: income},
